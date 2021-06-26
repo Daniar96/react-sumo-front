8098fd4bf4e9ff2984aeb4741d66f8618cca25f8
wester000 <veen.born@gmail.com>
2021-06-26 15:46:37 +0200

    added COALESCE everywhere

--- src/backend/src/main/java/com/geosumo/teamone/queries/DataAggregationQueries.java
+++ src/backend/src/main/java/com/geosumo/teamone/queries/DataAggregationQueries.java
@@ -2,7 +2,7 @@ package com.geosumo.teamone.queries;
 
 public class DataAggregationQueries {
     public static final String GET_SIMULATIONS =
-            "SELECT to_jsonb(array_agg(jsonobj.simdata)) " +
+            "SELECT COALESCE(to_jsonb(array_agg(jsonobj.simdata)), '[]') " +
                     "FROM simulation s, " +
                     "     LATERAL (SELECT json_build_object('id', s.id, 'name', s.name, 'date', s.upload_date, 'description', " +
                     "                                       s.description, 'steps', count(DISTINCT o.time_step)) simdata " +
@@ -10,18 +10,18 @@ public class DataAggregationQueries {
                     "              WHERE o.sim_id = s.id) jsonobj;";
 
     public static final String GET_SIMULATION_BY_ID =
-            "SELECT jsonb_build_object('id', s.id, " +
+            "SELECT COALESCE(jsonb_build_object('id', s.id, " +
                     "                          'name', s.name, " +
                     "                          'date', s.upload_date, " +
                     "                          'description', s.description, " +
-                    "                          'steps', count(DISTINCT o.time_step)) " +
+                    "                          'steps', count(DISTINCT o.time_step)), '[]') " +
                     "FROM simulation s, " +
                     "     output o " +
                     "WHERE s.id = ? " +
                     "  AND o.sim_id = ? " +
                     "GROUP BY s.id;";
 
-    public static final String GET_TIME_STAMP = "SELECT jsonb_agg(data) \n" +
+    public static final String GET_TIME_STAMP = "SELECT COALESCE(jsonb_agg(data), '[]') \n" +
             "FROM (\n" +
             "SELECT time_step, jsonb_agg(json_build_object('id', vehicle_id, 'x', x, 'y', y, 'angle', angle, 'type', type, 'spd', speed, 'pos', pos, 'lane', lane, 'slope', slope)) as vehicles\n" +
             "FROM geo_sumo.output out\n" +
@@ -31,13 +31,13 @@ public class DataAggregationQueries {
             "GROUP BY time_step ) as data";
 
     public static final String GET_ALL_NODES =
-            "SELECT to_jsonb(array_agg(nodes.data)) " +
+            "SELECT COALESCE(to_jsonb(array_agg(nodes.data)), '[]') " +
                     "FROM node, " +
                     "     LATERAL ( SELECT jsonb_build_object('id', node_id, 'lon', x, 'lat', y) AS data) nodes " +
                     "WHERE sim_id = ?";
 
     public static final String GET_ALL_EDGES =
-            "SELECT jsonb_agg(ultimateData.line)                    \n" +
+            "SELECT COALESCE(jsonb_agg(ultimateData.line), '[]')                    \n" +
                     "FROM (                                         \n" +
                     " SELECT json_build_object('id', e.edge_id, 'start', e.start, 'finish', e.finish,                                   \n" +
                     "                          'geometry', COALESCE(string_to_array(e.shape, ' '),  \n" +
@@ -52,7 +52,7 @@ public class DataAggregationQueries {
                     " ORDER BY e.edge_id) AS ultimateData;";
 
     public static final String GET_ALL_EDGES_WITH_COUNT = "" +
-            "SELECT jsonb_agg(ultimatedata)\n" +
+            "SELECT COALESCE(jsonb_agg(ultimatedata), '[]')\n" +
             "FROM(\n" +
             "SELECT data.*, COUNT(out.sim_id) AS count\n" +
             "FROM (\n" +
@@ -77,14 +77,15 @@ public class DataAggregationQueries {
             "ORDER BY count DESC) AS ultimateData";
 
 
-    public static final String VEHICLE_PER_TIME_STEP = "SELECT jsonb_agg(data) " +
+    public static final String VEHICLE_PER_TIME_STEP = "" +
+            "SELECT COALESCE(jsonb_agg(data), '[]') " +
             "            FROM ( SELECT time_step, count(*) AS vehicles " +
             "                    FROM output " +
             "                    WHERE sim_id = ? " +
             "                    GROUP BY time_step" +
             "                    ORDER BY time_step) AS data;";
 
-    public static final String SPEED_PER_TIME_STEP = "SELECT coalesce(jsonb_agg(data), '[]') " +
+    public static final String SPEED_PER_TIME_STEP = "SELECT COALESCE(jsonb_agg(data), '[]') " +
             "                    FROM ( " +
             "                    SELECT time_step, AVG(speed) as averagespeed " +
             "                    FROM output " +
@@ -93,7 +94,7 @@ public class DataAggregationQueries {
             "                    ORDER BY time_step) as data;";
 
     public static final String SLOWEST_VEHICLE_PER_TIME_STEP = "" +
-            "SELECT coalesce(jsonb_agg(data), '[]')                         \n" +
+            "SELECT COALESCE(jsonb_agg(data), '[]')                         \n" +
             "FROM ( SELECT vehicle_id AS ID, speed, x, y                    \n" +
             "       FROM output                                             \n" +
             "       WHERE sim_id = ?                                        \n" +
@@ -103,7 +104,7 @@ public class DataAggregationQueries {
             "       LIMIT 10 ) AS data;                                     \n";
 
     public static final String BUSIEST_ROADS_PER_TIME_STEP =
-            "SELECT jsonb_agg(data)                         \n" +
+            "SELECT COALESCE(jsonb_agg(data), '[]')                         \n" +
                     "FROM(                                  \n" +
                     " SELECT DISTINCT edge_id, count, (n1.x+n2.x)/2 as x, (n1.y+n2.y)/2 as y\n" +
                     " FROM(\n" +
