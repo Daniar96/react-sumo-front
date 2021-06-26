2f45c484ad4d63314cb6b53df1667ddfa9f84f0b
wester000 <veen.born@gmail.com>
2021-06-26 15:31:32 +0200

    added a bunch of synchronized

--- src/backend/src/main/java/com/geosumo/teamone/dao/SimulationDao.java
+++ src/backend/src/main/java/com/geosumo/teamone/dao/SimulationDao.java
@@ -20,44 +20,44 @@ import static com.geosumo.teamone.queries.DataInsertionQueries.INSERT_NEW_METADA
 public enum SimulationDao {
     INSTANCE;
 
-    public String getListJson() throws SQLException {
+    public synchronized String getListJson() throws SQLException {
         return getFromDatabasePrepared(GET_SIMULATIONS);
     }
 
-    public String getMetadataJson(int id) throws SQLException {
+    public synchronized String getMetadataJson(int id) throws SQLException {
         return getFromDatabasePrepared(GET_SIMULATION_BY_ID, id, id);
     }
 
-    public String getVehiclesJson(int id, int from, int to) throws SQLException {
+    public synchronized String getVehiclesJson(int id, int from, int to) throws SQLException {
         return getFromDatabasePrepared(GET_TIME_STAMP, id, from, to);
     }
 
-    public String getNodesJson(int id) throws SQLException {
+    public synchronized String getNodesJson(int id) throws SQLException {
         return getFromDatabasePrepared(GET_ALL_NODES, id);
     }
 
-    public String getEdgesJson(int id, int timestep) throws SQLException {
+    public synchronized String getEdgesJson(int id, int timestep) throws SQLException {
         return getFromDatabasePrepared(GET_ALL_EDGES_WITH_COUNT, id, id, id, id, timestep);
     }
 
-    public StaticGraphs getStaticGraphs(int id) throws SQLException {
+    public synchronized StaticGraphs getStaticGraphs(int id) throws SQLException {
         return new StaticGraphs(getFromDatabasePrepared(VEHICLE_PER_TIME_STEP, id), getFromDatabasePrepared(SPEED_PER_TIME_STEP, id));
     }
 
-    public DynamicGraphs getDynamicGraphs(int id, int timestep) throws SQLException {
+    public synchronized DynamicGraphs getDynamicGraphs(int id, int timestep) throws SQLException {
         return new DynamicGraphs(getFromDatabasePrepared(SLOWEST_VEHICLE_PER_TIME_STEP, id, timestep), getFromDatabasePrepared(BUSIEST_ROADS_PER_TIME_STEP, id, timestep));
     }
 
-    public void setMetadata(int id, String name, String description) throws SQLException{
+    public synchronized void setMetadata(int id, String name, String description) throws SQLException{
         String[] params = new String[] {name, description};
         updateDatabase(INSERT_NEW_METADATA, params, id);
     }
 
-    public void deleteSim(int id) throws SQLException {
+    public synchronized void deleteSim(int id) throws SQLException {
         updateDatabase(DELETE_SIMULATION, new String[0], id);
     }
 
-    public void createNewZipFile(String name, String description, ZipInputStream input) throws IOException, SQLException {
+    public synchronized void createNewZipFile(String name, String description, ZipInputStream input) throws IOException, SQLException {
         System.out.println("uplaoding " + name + " simulation...");
         StringBuilder sb = new StringBuilder();
         byte[] buffer = new byte[1024];
@@ -78,5 +78,6 @@ public enum SimulationDao {
         String[] s = new String[files.size()];
         files.toArray(s);
         Helpers.processFiles(name, description, s);
+
     }
 }
